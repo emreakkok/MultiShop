8a2ed6886dcecc149aea1935b695a32677b8bac4
emreakkok <151877912+emreakkok@users.noreply.github.com>
2025-06-29 21:36:57 +0300

    Proje dosyası ekle.

---
+++ Services/Discount/MultiShop.Discount/Migrations/20250626153647_mig_IsActive.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultiShop.Discount.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_IsActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "İsActive",
+                table: "Coupons",
+                newName: "IsActive");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Coupons",
+                newName: "İsActive");
+        }
+    }
+}
